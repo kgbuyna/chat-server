ed11c386023c667548a6429be3208dbf579d1bf7
Khuyag Buyan-Urt <buynaa.khuyag@gmail.com>
2025-03-25 18:22:49 +0800

    socket setup

---
+++ src/server.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  socket.on("message", (msg) => {
+    console.log("Message received: ", msg);
+    socket.emit("random", msg + " " + Math.random());
+  });
+
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+  });
+});
+
+server.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
